9cc02a2ab3f3e9b5073ab92bb87df68fca8aa163
yohku <4oh.black@gmail.com>
2026-06-01 13:06:03 +0900

    server

---
+++ src/build_lecture/src/exec_sample3/service_server.cpp
@@ -0,0 +1,38 @@
+#include<memory>
+#include<rclcpp/rclcpp.hpp>
+
+#include "example_interfaces/srv/add_two_ints.hpp"
+
+
+class MinimalService:public rclcpp::Node
+{
+    public:
+    MinimalService()
+    :Node("minimal_service")
+    {
+        service=this->create_service<example_interfaces::srv::AddTwoInts>(
+            "add_two_ints",
+            std::bind(&MinimalService::add,this.std::placeholders::_1,std::placeholders::_2)
+        );
+
+        RCLCPP_INFO(this->get_logger(),"Ready to add two ints.");
+    }
+    private:
+    void add(const std::Shared_ptr<example_interfaces::srv::AddTwoInts::Request>request,
+    std;;Shared_ptr<example_interfaces::srv::AddTwoInts::Response>response
+    ){
+        response->sum=request->a+request->b;
+
+        RCLCPP_INFO(this->get_logger(), "Incoming request\na: %ld b: %ld", request->a, request->b);
+        RCLCPP_INFO(this->get_logger(), "Sending back response: [%ld]", (long int)response->sum);
+    }
+    rclcpp::Service<example_interfaces::srv::AddTwoInts>:SharedPtr service;
+};
+
+int main(int argc,char **argv)
+{
+    rclcpp::init(argc,argv);
+    rclcpp::spin(std::make_shared<MinimalService>();
+    rclcpp::shutdown();
+    return 0;
+}
