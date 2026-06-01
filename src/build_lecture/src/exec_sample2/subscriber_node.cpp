bd82bcd9b06f6aadfba4df0ce1d3ad926993f107
yohku <4oh.black@gmail.com>
2026-06-01 11:09:40 +0900

    wip

--- src/build_lecture/src/exec_sample2/subscriber_node.cpp
+++ src/build_lecture/src/exec_sample2/subscriber_node.cpp
@@ -5,8 +5,8 @@
 class MinimalSubscriber : public rclcpp::Node
 {
     public:
-    MinimalSubscriber()
-    :Node("minimal_subsciber")
+    MinimalSubscriber(const rclcpp::NodeOptions & options)
+    :Node("minimal_subsciber",options)
     {
         subscription = this->create_subscription<std_msgs::msg::String>
         (
@@ -22,10 +22,32 @@ class MinimalSubscriber : public rclcpp::Node
         rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription;
 };
 
+//int main(int argc,char *argv[])
+//{
+//    rclcpp::init(argc,argv);
+//    rclcpp::spin(std::make_shared<MinimalSubscriber>());
+//    rclcpp::shutdown();
+//    return 0;
+//}
+
 int main(int argc,char *argv[])
 {
     rclcpp::init(argc,argv);
-    rclcpp::spin(std::make_shared<MinimalSubscriber>());
+    // 1. プロセス内通信を「有効（true）」にしたオプションを用意する
+    rclcpp:NodeOptions options;
+    options.use_intra_process_comms(true);
+// 2. 同じオプションを使って、両方のノードのインスタンスを「同じメイン関数内」で作る
+    auto talker_node=std::make_shared<MinimalPublisher>(options);
+    auto listener_node=std::make_shared<MinimalSubscriber>(options);
+
+    // 3. 「Executor（エグゼキュータ）」というまとめ役を用意する
+    rclcpp::executors::SingleThreadedRxecutor executor;
+
+    executor.add_node(talker_node);
+    executor.add_node(listener_node);
+
+    executor.spin();
+    
     rclcpp::shutdown();
     return 0;
 }
